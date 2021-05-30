7ac292698d674d0b43ef9fa637419516778f176e
dale563 <limogesdale@outlook.com>
2021-05-30 12:26:11 -0400

    Creation du panier, pauffinage CSS

--- src/assets/scripts/boutique.js
+++ src/assets/scripts/boutique.js
@@ -1,1 +1,19 @@
 console.log('hello world!');
+
+// let nombreArticlesDansLePanier = 0;
+
+// function ouvrirLePanier () {
+
+// }
+
+// function fermerLePanier () {
+
+// }
+
+// function ajouterAuPanier (produitAAjoute) {
+
+// }
+
+// function retirerDuPanier (produitARetirer) {
+
+// }
