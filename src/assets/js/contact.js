44af0e7442cf12393297a58e9a0f6877a1a316d5
Sabrina Sylvestre <2096949@cegeplimoilou.ca>
2021-06-09 16:08:32 -0400

    dernières vérifs

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -1,7 +1,6 @@
-const motDePasseInput = document.getElementById('mot_de_passe');
-const boutonSoumettre = document.getElementById('soumettre');
 const formElement = document.querySelector('form');
 const motDePasseFieldset = document.querySelector('.mot_de_passe'); 
+const motDePasseInput = document.getElementById('mot_de_passe');
 
 let messageNonActive = true; 
 let pMessageErreur = document.createElement('p');
@@ -13,12 +12,12 @@ function afficherMessageErreur(event) {
 		event.preventDefault();
 		
 		if (messageNonActive) { 
-			pMessageErreur.innerHTML = 'Le mot de passe n\'est pas valide'
+			pMessageErreur.innerHTML = 'Le mot de passe n\'est pas valide';
 			motDePasseFieldset.appendChild(pMessageErreur); 
 			pMessageErreur.classList.add('message_erreur'); 
-			messageNonActive = false; 
-		}; 
-	};
+			messageNonActive = false;
+		}
+	}
 }
 
-formElement.addEventListener('submit', afficherMessageErreur)
+formElement.addEventListener('submit', afficherMessageErreur);
